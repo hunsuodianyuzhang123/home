b1987cc84b8865ad415bced995dc2f7d3195fd06
admin <admin@qq.com>
2021-12-14 16:37:37 +0800

    又做了一些大改

--- js/music.js
+++ js/music.js
@@ -27,3 +27,14 @@ $(function () {
   console.log('以后有可能会完善')
   console.log('随便写的，有什么问题的话可别找我哦~~')
 })
+
+// //
+// if (
+//   navigator.userAgent.match(
+//     /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
+//   )
+// ) {
+//   window.location.href = './music.html' // 手机
+// } else {
+//   window.location.href = './index.html' // 电脑
+// }
